758a0b72d4a8679fc55b172e4c804897cb3c607f
MaKu <andrmatgonros@gmail.com>
2025-05-24 00:21:35 +0200

    Client refactorizado y comentado

--- Code/Client/Client.cpp
+++ Code/Client/Client.cpp
@@ -1,245 +1,288 @@
 #pragma comment(lib, "ws2_32.lib")
 
-#include <stdio.h>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <assert.h>
-#include "../Lib/Lib.h"
-#include "../Game/Game.hpp"
 #include <iostream>
 #include <string>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <Windows.h>
+#include <assert.h>
+
+#include "../Lib/Lib.h"
+#include "../Game/Game.hpp"
 
 using namespace std;
 
-int obtainNewPort(SOCKET s, sockaddr_in* server_addr, string prefix);
-
+// Pauses and clears the console screen
 void clearScreen()
 {
     system("pause");
     system("CLS");
 }
-int main(int argc, char* argv[])
-{
-    if (argc < 2) {
-        cout << "UDPClient usage: .\\UDPClient <id>" << endl;
-        ExitProcess(-1);
-    }
-    int client = atoi(argv[1]);
-    std::cout << "Client: starting..." << std::endl;
-    //required intialization of WinSock 2 library, it writes some data om wsaData to check everything is ok
-    int result;
-    WSAData wsaData;
-    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    assert(result == NO_ERROR);
-    std::cout << "Client: WinSock started correctly" << std::endl;
 
-    //now we create a socket that uses IP (AF_INET) with UDP (SOCK_DGRAM and IPPROTO_UDP) 
+// Displays sonar warning messages based on nearby treasures or traps
+void displaySonarWarning(bool treasureNearby, bool trapNearby)
+{
+    if (trapNearby && treasureNearby)
+    {
+        cout << "Be careful! There are traps and treasures nearby!" << endl;
+    }
+    else if (trapNearby)
+    {
+        cout << "Danger! Trap nearby!" << endl;
+    }
+    else if (treasureNearby)
+    {
+        cout << "Treasure nearby! Keep looking." << endl;
+    }
+    else
+    {
+        cout << "No signal of treasures nor traps nearby." << endl;
+    }
+}
+
+// Requests a new port from the server and updates the server address accordingly
+int obtainNewPort(SOCKET s, sockaddr_in* serverAddr, const string& prefix)
+{
+    PDataPacket request = new DataPacket();  // Create an empty request packet
+    PDataPacket response = new DataPacket(); // Packet to receive server's response
+    sendtorecvfromMsg(s, serverAddr, request, response, prefix);
+    return 0;
+}
+
+// Initializes WinSock library for network communication
+void initializeWinSock()
+{
+    WSAData wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    assert(result == NO_ERROR);
+    cout << "Client: WinSock started correctly" << endl;
+}
+
+// Creates and returns a UDP socket for communication
+SOCKET createSocket()
+{
     SOCKET s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (s == INVALID_SOCKET) {
+    if (s == INVALID_SOCKET)
+    {
         treatErrorExit("Client: Socket creation error: ", s, -1);
     }
-    std::cout << "Client: socket created" << std::endl;
-    //now we specify the other machine we want to send messages to
-    sockaddr_in server_addr;
-    PCSTR address = "127.0.0.1";
-    if (!inet_pton(AF_INET, address, &(server_addr.sin_addr.s_addr))) { // Replace with your desired IP address
-        printf("error converting IP in string to binary\n");
-        return -1;
-    }
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(4000);
+    return s;
+}
 
-    string prefix = "Client:";
-    obtainNewPort(s, &server_addr, prefix);
-    std::cout << "Client already obtained new port: " << ntohs(server_addr.sin_port) << std::endl;
-    enum CellInfo cI = NOTHING; // To use cellInfo later
-    // Creates a game with the map's width, height and the max number of turns (has to be the same as server's)
-    Game game(WIDTH, HEIGHT, TURNS); 
-    system("CLS");
+// Configures server address struct with given IP and port
+sockaddr_in configureServerAddress(const char* ip, int port)
+{
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &(serverAddr.sin_addr.s_addr));
+    return serverAddr;
+}
+
+// Closes the socket and cleans up WinSock resources
+void shutdownClient(SOCKET s)
+{
+    if (closesocket(s) == SOCKET_ERROR)
+    {
+        cerr << "closesocket failed with error: " << WSAGetLastError() << endl;
+    }
+    WSACleanup();
+    cout << "Client finishing..." << endl;
+}
+
+// Gets the player's action and related input (displacement or sonar direction)
+int getPlayerAction(int& x, int& y, char& dir)
+{
+    int action;
+    cin >> action;
+
+    if (action == 1)
+    {
+        cout << "x displacement: "; cin >> x;
+        cout << "y displacement: "; cin >> y;
+    }
+    else if (action == 7)
+    {
+        cout << "Choose a direction to fire the sonar (N/S/E/W): ";
+        cin >> dir;
+        dir = toupper(dir);
+    }
+
+    return action;
+}
+
+// Handles the response from the server based on the player's action
+void handleServerResponse(Game& game, Player& player, Map& map, PDataPacket response, int action, CellInfo& cellInfo, bool& treasureNearby, bool& trapNearby, bool& sonar, bool& isRunning)
+{
+    Cell cell = map.getCell(player.getPosition().x, player.getPosition().y);
+
+    switch (action)
+    {
+    case 2:
+    {
+        // Check if the cell is dug or contains a flag
+        if (response->isDug) cout << "Cell has already been dug." << endl;
+        if (response->cellInfo == FLAG) cout << "There's a flag here." << endl;
+        if (!response->isDug) cout << "Cell hasn't been dug." << endl;
+        break;
+    }
+
+    case 3:
+    {
+        // Update cell info and map with the server response
+        cellInfo = response->cellInfo;
+        cell.isDug = response->isDug;
+        map.setCell(player.getPosition().x, player.getPosition().y, cell);
+        game.setMap(map);
+
+        // Provide feedback depending on cell content
+        switch (cellInfo)
+        {
+        case NOTHING:
+        {
+            cout << "There's nothing here." << endl;
+            break;
+        }
+        case TREASURE:
+        {
+            cout << "Treasure found!" << endl;
+            game.setTreasuresFound(response->treasuresFound);
+            break;
+        }
+        case TRAP:
+        {
+            cout << "It's a trap!" << endl;
+            break;
+        }
+        default:
+            break;
+        }
+
+        break;
+    }
+
+    case 4:
+    {
+        // Show sonar warnings about nearby traps or treasures
+        displaySonarWarning(treasureNearby, trapNearby);
+        break;
+    }
+
+    case 5:
+    {
+        // Mark the cell if a flag is detected
+        if (response->cellInfo == FLAG)
+        {
+            cell.hasFlag = true;
+            map.setCell(player.getPosition().x, player.getPosition().y, cell);
+            game.setMap(map);
+        }
+        break;
+    }
+
+    case 6:
+    {
+        // Notify player of energy replenishment
+        cout << "You replenished your energy. Current energy: " << player.getEnergy() << endl;
+        break;
+    }
+
+    case 7:
+    {
+        // Output sonar detection result
+        cout << (sonar ? "The sonar detects something in that direction." :
+            "Nothing detected in that direction.") << endl;
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    clearScreen();
+
+    // Update flags based on server response
+    treasureNearby = response->treasureNearby;
+    trapNearby = response->trapNearby;
+    sonar = response->sonar;
+    isRunning = response->isRunning;
+}
+
+// Main game loop handling input, communication, and state updates
+void runGameLoop(SOCKET s, sockaddr_in& serverAddr, const string& prefix, Game& game, int clientId)
+{
     bool isRunning = true;
-    int treasuresFound = 0;
-    Player player = game.getPlayer();
     bool treasureNearby = false, trapNearby = false, sonar = false;
+    int treasuresFound = 0;
+    CellInfo cellInfo = NOTHING;
+    Player player = game.getPlayer();
+
     while (isRunning)
     {
-        game.run(); // Shows board and menu
-        int action;
-        cin >> action; // Inputs action
+        game.run();
+
         int x = 0, y = 0;
-        if (action == 1) // If action is move, inputs x and y displacement
-        {
-            cout << "x displacement: ";
-            cin >> x;
-            cout << "y displacement: ";
-            cin >> y;
-        }
         char dir = ' ';
-        if (action == 7) // If action is sonar, chooses direction
-        {
-            cout << "Choose a direction to fire the sonar (N/S/E/W): ";
-            cin >> dir;
-            dir = toupper(dir);
-        }
-        // Creates packet with necessary information to send to server (action - 1 because enums start at 0)
-        PDataPacket packet = new DataPacket(client, static_cast<Operation>(action - 1), cI,
-            treasureNearby, trapNearby, sonar,
-            dir, player.getEnergy(), x, y,
-            game.getCurrentTurn(), game.getTurnLimit(), treasuresFound,
-            game.getIsRunning(), game.getPlayer().getPosition(), false);
-        // Allocates for response
+        int action = getPlayerAction(x, y, dir);
+
+        // Prepare request packet to send to server
+        PDataPacket request = new DataPacket(clientId, static_cast<Operation>(action - 1), cellInfo,
+            treasureNearby, trapNearby, sonar, dir,
+            player.getEnergy(), x, y, game.getCurrentTurn(),
+            game.getTurnLimit(), treasuresFound,
+            game.getIsRunning(), player.getPosition(), false);
+
         PDataPacket response = new DataPacket();
-        sendtorecvfromMsg(s, &server_addr, packet, response, prefix);
-        if (action == 1) // Sets player position if action is move
+        sendtorecvfromMsg(s, &serverAddr, request, response, prefix);
+
+        if (action == 1)
         {
             player.setPosition(response->position);
         }
-        // Sets all other values
+
         player.setEnergy(response->energy);
         game.setPlayer(player);
         game.setCurrentTurn(response->currentTurn);
-        treasureNearby = response->treasureNearby;
-        trapNearby = response->trapNearby;
-        sonar = response->sonar;
-        isRunning = response->isRunning;
+
         Map map = game.getMap();
-        Cell cell = map.getCell(player.getPosition().x, player.getPosition().y);
-		switch (action)
-		{
-            // Message for when player inspects the cell he's standing on
-		    case 2:
-		    {
-			    if (response->isDug)
-			    {
-				    cout << "Cell has already been dug." << endl;
-			    }
-                if (response->cellInfo == FLAG)
-                {
-                    cout << "There's a flag here." << endl;
-                    break;
-                }
-			    if (!response->isDug)
-			    {
-				    cout << "Cell hasn't been dug." << endl;
-			    }
-		    }
-		    break;
-
-            // When player digs cell
-            case 3:
-            {
-                // Receives cellInfo value
-                cI = response->cellInfo;
-                // Changes boolean
-                cell.isDug = response->isDug;
-
-                // Sets cell in the copied map
-                map.setCell(player.getPosition().x, player.getPosition().y, cell);
-
-                // Sets map in the game
-                game.setMap(map);
-
-                // Prints something depending on the information
-                switch (cI)
-                {
-                case NOTHING:
-                {
-                    cout << "There's nothing here." << endl;
-                }
-                break;
-                case TREASURE:
-                {
-                    cout << "Treasure found!" << endl;
-                    game.setTreasuresFound(response->treasuresFound);
-                }
-                break;
-                case TRAP:
-                {
-                    cout << "It's a trap!" << endl;
-                }
-                break;
-                }
-            }
-            break;
-
-            // Messages for when the player uses the map
-            case 4:
-            {
-                if (trapNearby && treasureNearby)
-                {
-                    cout << "Be careful! There are traps and treasures nearby!" << endl;
-                }
-                else if (trapNearby)
-                {
-                    cout << "Danger! Trap nearby!" << endl;
-                }
-                else if(treasureNearby)
-                {
-                    cout << "Treasure nearby! Keep looking." << endl;
-                }
-                else
-                {
-                    cout << "No signal of treasures nor traps nearby." << endl;
-                }
-            }
-            break;
-
-            // Flags the cell where the player is currently standing on
-            case 5:
-            {
-                if (response->cellInfo == FLAG)
-                {
-                    cell.hasFlag = true;
-                    map.setCell(player.getPosition().x, player.getPosition().y, cell);
-                    game.setMap(map);
-                }
-            }
-            break;
-
-            // Eats
-            case 6:
-            {
-                cout << "You replenished your energy. Current energy: " << player.getEnergy() << endl;
-            }
-            break;
-
-            // Uses sonar
-            case 7:
-            {
-                if (sonar)
-                {
-                    cout << "The sonar detects something in that direction." << endl;
-                }
-                else
-                {
-                    cout << "Nothing detected in that direction." << endl;
-                }
-            }
-            break;
-		}
-        clearScreen();
+        handleServerResponse(game, player, map, response, action, cellInfo, treasureNearby, trapNearby, sonar, isRunning);
     }
-    // Shows the amount of treasures player has revealed
-    game.getMap().displayMap(player.getPosition());
-    cout << "Game finished. Treasures found: " << treasuresFound << endl;
-    std::cout << "Client finishing..." << std::endl;
-    int iResult = closesocket(s);
-    if (iResult == SOCKET_ERROR) {
-        wprintf(L"closesocket failed with error: %d\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
-    }
-
-    WSACleanup();
-    return 0;
 }
 
-//sends first msg to server and returns with the new server_addr used for the server for the dedicated socket
-int obtainNewPort(SOCKET s, sockaddr_in* server_addr, string prefix) {
-    PDataPacket packet = new DataPacket(); //I don't initialize because the server won't care
-    PDataPacket response = new DataPacket();
-    //IMPORTANT: will overwrite server_addr with the server addr with the new port, since the response msg in the server is sent through the new socket!
-    sendtorecvfromMsg(s, server_addr, packet, response, prefix);
+int main(int argc, char* argv[])
+{
+    // Check for client ID argument
+    if (argc < 2)
+    {
+        cout << "Usage: ./UDPClient <id>" << endl;
+        return -1;
+    }
+
+    int clientId = atoi(argv[1]);
+    cout << "Client: starting..." << endl;
+
+    // Initialize networking
+    initializeWinSock();
+    SOCKET s = createSocket();
+
+    // Set server address to localhost and port 4000
+    sockaddr_in serverAddr = configureServerAddress("127.0.0.1", 4000);
+
+    string prefix = "Client:";
+
+    // Obtain new port from server
+    obtainNewPort(s, &serverAddr, prefix);
+    cout << "New port obtained: " << ntohs(serverAddr.sin_port) << endl;
+
+    // Initialize game instance
+    Game game(WIDTH, HEIGHT, TURNS);
+    system("CLS");
+
+    // Run main game loop
+    runGameLoop(s, serverAddr, prefix, game, clientId);
+
+    // Cleanup and close
+    shutdownClient(s);
+
     return 0;
 }
-
